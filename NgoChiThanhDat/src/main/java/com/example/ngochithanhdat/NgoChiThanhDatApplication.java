b8d8b9517a25a2250ce13ad1ad4dc53b397ba909
ngochithanhdat <dat3578@gmail.com>
2022-08-10 18:45:44 +0700

    up

---
+++ NgoChiThanhDat/src/main/java/com/example/ngochithanhdat/NgoChiThanhDatApplication.java
@@ -0,0 +1,13 @@
+package com.example.ngochithanhdat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoChiThanhDatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgoChiThanhDatApplication.class, args);
+    }
+
+}
---
+++ NgoChiThanhDat/src/test/java/com/example/ngochithanhdat/NgoChiThanhDatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ngochithanhdat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoChiThanhDatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
